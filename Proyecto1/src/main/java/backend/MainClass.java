ece8d16befdd023ae15e1276876fd485daf9efea
= <jeffrey.menendez.castillo@gmail.com>
2020-08-04 19:36:52 -0600

    Ingreso

---
+++ Proyecto1/src/main/java/backend/MainClass.java
@@ -0,0 +1,18 @@
+
+package backend;
+
+import gui.MainFrame;
+
+/**
+ *
+ * @author jeffrey
+ */
+public class MainClass {
+     public static void main(String[] args) {
+
+        System.out.println("Bienvenido");
+         MainFrame interfaz= new MainFrame();
+         interfaz.setVisible(true);
+        
+    }
+}
